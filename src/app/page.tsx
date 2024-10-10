db67efd23f0750244393730b7c783263fd02c220
HadohhhKen <hadouken@chaintheseeds.com>
2024-10-10 20:36:16 +0900

    main: CSR sample

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,83 +1,84 @@
+'use client';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{' '}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [data, setData] = useState<any>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_=${Date.now()}`);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const result = await response.json();
+        setData(result.slice(0, 20));
+      } catch (error) {
+        console.log('error', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  //if (loading) return <p>Loading...</p>;
+
+  return (
+    <>
+      <header className="bg-slate-600">
+        <div className="flex justify-between items-center w-[95%] max-w-[1280px] mx-auto text-white">
+          <a className="header__logo" href="/">
+            Site Logo
           </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <div className="header__menu">
+            <nav>
+              <ul>
+                <li>
+                  <a className="block py-5" href="/sub/">
+                    Sub
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
+      </header>
+      <main className="w-[95%] max-w-[1000px] mx-auto py-10">
+        <ul className="grid grid-cols-3 gap-5 min-h-[100vh]">
+          {!loading &&
+            data?.map((item: any) => (
+              <li className="bg-slate-100 p-5 grid gap-1" key={item.id}>
+                <div className="font-semibold">{item.title}</div>
+                <div>{item.body}</div>
+              </li>
+            ))}
+        </ul>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image aria-hidden src="https://nextjs.org/icons/file.svg" alt="File icon" width={16} height={16} />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image aria-hidden src="https://nextjs.org/icons/window.svg" alt="Window icon" width={16} height={16} />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image aria-hidden src="https://nextjs.org/icons/globe.svg" alt="Globe icon" width={16} height={16} />
-          Go to nextjs.org →
-        </a>
+      <footer className="bg-slate-600">
+        <div className="grid place-items-center gap-2 w-[95%] max-w-[1280px] mx-auto py-5 text-white">
+          <nav>
+            <ul className="flex gap-2">
+              <li>
+                <a href="#">
+                  <Image src="/assets/images/common/icon-x.svg" alt="" width={40} height={40} />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <Image src="/assets/images/common/icon-fb.svg" alt="" width={40} height={40} />
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <p>
+            <small>©hoo bar baz...</small>
+          </p>
+        </div>
       </footer>
-    </div>
+    </>
   );
 }
