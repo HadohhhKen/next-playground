4f2bbb9155193f1a6970b4207c28637bc209b0b5
HadohhhKen <hadouken@chaintheseeds.com>
2024-10-11 14:21:07 +0900

    main: img to Image

--- src/app/sub/page.tsx
+++ src/app/sub/page.tsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
+import Image from 'next/image';
+
 export default function Home() {
   const [data, setData] = useState<any>([]);
   const [loading, setLoading] = useState<boolean>(true);
@@ -32,16 +34,17 @@ export default function Home() {
     <>
       <Header />
       <main className="w-[95%] max-w-[1000px] mx-auto py-10">
-        <ul className="grid grid-cols-5 gap-5 min-h-[100vh]">
+        <ul className="grid grid-cols-5 gap-6 min-h-[100vh]">
           {!loading &&
             data?.map((item: any) => (
               <li className="bg-slate-100 w-[100%] h-0 pt-[100%] relative" key={item.id}>
-                <img
+                <Image
                   className="absolute top-0 left-0 w-[100%] h-[100%] object-contain"
                   src={item.thumbnailUrl}
                   alt=""
                   width={150}
                   height={150}
+                  unoptimized
                 />
               </li>
             ))}
