4c06c0525aa278cf884636eb28253e2612e703b9
HadohhhKen <hadouken@chaintheseeds.com>
2024-10-11 11:25:55 +0900

    main: next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  basePath: isProd ? '/next-playground' : '',
+  trailingSlash: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+        port: '',
+        pathname: '/**/*',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
