4cd240c631e1cfc18a34ec7929a04ea80eb5bab2
HadohhhKen <hadouken@chaintheseeds.com>
2024-10-11 17:43:48 +0900

    main: downscale image

--- src/app/sub/page.tsx
+++ src/app/sub/page.tsx
@@ -29,24 +29,24 @@ export default function Home() {
   }, []);
 
   //if (loading) return <p>Loading...</p>;
+  console.log('🚀 ~ Home ~ loading:', loading);
 
   return (
     <>
       <Header />
       <main className="w-[95%] max-w-[1000px] mx-auto py-10">
         <ul className="grid grid-cols-4 gap-6 min-h-[100vh]">
-          {!loading &&
-            data?.map((item: any) => (
-              <li className="bg-slate-100 w-[100%] h-0 pt-[100%] relative" key={item.id}>
-                <Image
-                  className="absolute top-0 left-0 w-[100%] h-[100%] object-contain"
-                  src={item.download_url}
-                  alt=""
-                  width={500}
-                  height={500}
-                />
-              </li>
-            ))}
+          {data?.map((item: any) => (
+            <li className="bg-slate-100 w-[100%] h-0 pt-[100%] relative" key={item.id}>
+              <Image
+                className="absolute top-0 left-0 w-[100%] h-[100%] object-contain"
+                src={item.download_url}
+                alt=""
+                width={item.width / 10}
+                height={item.height / 10}
+              />
+            </li>
+          ))}
         </ul>
       </main>
       <Footer />
