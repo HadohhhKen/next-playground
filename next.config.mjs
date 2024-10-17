701a12097863f54fce0e51e72de59f89c50e568a
HadohhhKen <hadouken@chaintheseeds.com>
2024-10-17 20:15:18 +0900

    main: TEST fixing build error

--- next.config.mjs
+++ next.config.mjs
@@ -3,17 +3,14 @@ import withExportImages from 'next-export-optimize-images';
 const isProd = process.env.NODE_ENV === 'production';
 const basePath = isProd ? '/next-playground' : '';
 
-const createNextConfig = async () => {
-  const nextConfig = await withExportImages({
-    output: 'export',
+const nextConfig = withExportImages({
+  output: 'export',
+  basePath,
+  assetPrefix: basePath,
+  trailingSlash: true,
+  env: {
     basePath,
-    assetPrefix: basePath,
-    trailingSlash: true,
-    env: {
-      basePath,
-    },
-  });
-  return nextConfig;
-};
+  },
+});
 
-export default createNextConfig();
+export default nextConfig;
