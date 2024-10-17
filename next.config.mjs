ccad81df614672be73d7dea1c2a3a3f89a34355d
HadohhhKen <hadouken@chaintheseeds.com>
2024-10-17 19:34:55 +0900

    static-export: code cleanup

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ const isProd = process.env.NODE_ENV === 'production';
 const basePath = isProd ? '/next-playground' : '';
 
 const nextConfig = withExportImages({
-  //
   output: 'export',
   basePath,
   assetPrefix: basePath,
