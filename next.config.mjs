151a9b58bd04501fb066223ab032aa36d8c2214a
HadohhhKen <hadouken@chaintheseeds.com>
2024-10-17 19:45:48 +0900

    main: consider Promise

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,17 @@ import withExportImages from 'next-export-optimize-images';
 const isProd = process.env.NODE_ENV === 'production';
 const basePath = isProd ? '/next-playground' : '';
 
-const nextConfig = withExportImages({
-  output: 'export',
-  basePath,
-  assetPrefix: basePath,
-  trailingSlash: true,
-  env: {
+const createNextConfig = async () => {
+  const nextConfig = await withExportImages({
+    output: 'export',
     basePath,
-  },
-});
+    assetPrefix: basePath,
+    trailingSlash: true,
+    env: {
+      basePath,
+    },
+  });
+  return nextConfig;
+};
 
-export default nextConfig;
+export default createNextConfig();
