b3fdbdd5dc864b0c95694559e48a4fc078811e0d
HadohhhKen <hadouken@chaintheseeds.com>
2024-10-18 19:28:44 +0900

    Refactor basePath handling for multiple deployment patterns
    - Added nonRootDeployPath for non-root deployments.
    - Introduced isRootDeploy flag to simplify deployment path logic.
    - Updated comments to clarify deployment scenarios.#

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 import withExportImages from 'next-export-optimize-images';
-const isDev = process.env.NODE_ENV === 'development';
-const basePath = isDev ? '' : '/next-playground';
+
+/**
+ * There are multiple deployment patterns, and if deployment to a non-root path is necessary,
+ * please specify the path as needed.
+ * If you do not consider deployment other than the root, please comment out the following line.
+ */
+const nonRootDeployPath = '/next-playground';
+const isRootDeploy = process.env.NODE_ENV === 'development' || process.env.VERCEL || process.env.NETLIFY;
+const basePath = isRootDeploy ? '' : nonRootDeployPath || '';
+/**
+ * If defining basePath is necessary, the same value needs to be set in export-images.config.js (CommonJS),
+ * so it is stored in an environment variable.
+ */
 process.env.BASE_PATH = basePath;
 
 const nextConfig = withExportImages({
